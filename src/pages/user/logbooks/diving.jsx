97af67f36df64308ea7a4872d4a328e2265c9894
Sean Knowles <s@seanknowles.dev>
2021-07-06 23:38:20 +0100

    added nvm and fixed no react component empty file errors

---
+++ src/pages/user/logbooks/diving.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function diving() {
+  return <div>Diving Logbook</div>
+}
---
+++ src/pages/user/logbooks/supervisor.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function supervisor() {
+  return <div>Supervisor</div>
+}
